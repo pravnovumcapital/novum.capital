50a1ab646b489adbf460d134581844272af84258
pravnovumcapital <praveen@novum.capital>
2019-01-07 13:38:04 +0530

    Contact Form

---
+++ mail.php
@@ -0,0 +1,12 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$company = $_POST['company'];
+$number = $_POST['number'];
+$helptext = $_POST['helptext'];
+$formcontent="From: $name \n Contact Number: $number \n Company: $company \n Message: $helptext";
+$recipient = "ask@novum.capital";
+$subject = "Novum Capital Contact Request";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank you for contacting us! We will get back to you soon!";
