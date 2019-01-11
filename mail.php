a8827e7fb181402dc7ee564857787394a1db9231
pravnovumcapital <praveen@novum.capital>
2019-01-11 14:46:53 +0530

    reomove sendgrid

--- mail.php
+++ mail.php
@@ -14,23 +14,4 @@ $formcontent="From: $name \n Contact Number: $number \n Company: $company \n Mes
 
 
 
-require './vendor/autoload.php'; // If you're using Composer (recommended)
 
-$email = new \SendGrid\Mail\Mail(); 
-$email->setFrom("ask@novum.capital", "Example User");
-$email->setSubject("Novum Capital Contact Request");
-$email->addTo("praveen@novum.capital", "praveen");
-$email->addContent("text/plain", "We have a new contact request");
-$email->addContent(
-    "text/html", "<strong>".$formcontent."</strong>"
-);
-$sendgrid = new \SendGrid(getenv('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'));
-try {
-    $response = $sendgrid->send($email);
-    //print $response->statusCode() . "\n";
-    //print_r($response->headers());
-    //print $response->body() . "\n";
-    echo "Thank you for contacting us! We will get back to you soon!";
-} catch (Exception $e) {
-    echo 'Caught exception: '. $e->getMessage() ."\n";
-}
